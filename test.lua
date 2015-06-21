075bfb68931fafc4e17291164e28a7ce82794f94
rocaltair <rocaltair@gmail.com>
2015-06-21 12:01:48 +0800

    update test.lua

--- test.lua
+++ test.lua
@@ -1,8 +1,28 @@
-gpio = require "lwiringpi"
+lwiringpi = require "lwiringpi"
 
-for k, v in pairs(gpio) do
-	print(k, v)
+
+print("Macros:")
+
+local array = {}
+for k, v in pairs(lwiringpi) do
+	table.insert(array, {key = k, value = v})
 end
-gpio.delay(5000)
+
+table.sort(array, function(a, b)
+	return a.key < b.key
+end)
+
+for _, data in pairs(array) do
+	local k = data.key
+	local v = data.value
+	if type(v) ~= "function" then
+		print(string.format("lwiringpi.%s", k))
+	else
+		print(string.format("lwiringpi.%s()", k))
+	end
+end
+
+
+
 
 
